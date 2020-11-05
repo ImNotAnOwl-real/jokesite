0e1abb9bdeff4fda7da9ea36cdff137d36b52c75
Frederik Holmboe <eaaftho@students.eaaa.dk>
2020-11-05 09:20:31 +0100

    hej

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
     databaseURI: 'mongodb+srv://imnotanowl:lowbob123@fredescluster.b0brk.mongodb.net/jokeService?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
     localPort: 8090
-//
+////
 };
 
 module.exports = config;    
